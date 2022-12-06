e97800eaf91a76305dd6c44051231a3cfc0f902e
Antoine Bollinger <antoine.bollinger@gmail.com>
2022-12-06 10:40:29 +0100

    ⚡️ improving Route main class

--- config/Router.class.php
+++ config/Router.class.php
@@ -9,14 +9,21 @@ class Router
     protected $routes;
     protected $texts;
 
+    /**
+     * Base class for any router need
+     * 
+     * @param string $path          The path required for the router
+     * @param string|array $routes  Can be an single string pathname the a yaml file containing the routes OR an array of multiples files pathnames
+     * @param string|array $texts   As the routes, this can be a single string pathname the a yaml containing the texts used, OR an arrayof multiples files pathnames
+     */
     public function __construct(
-        $path = "",
-        $routesPath = "",
-        $textsPath = ""
+        $path = null,
+        $routes = null,
+        $texts = null
     ) {
         $this->setPath($path);
-        $this->setRoutes($routesPath);
-        $this->setTexts($textsPath);
+        $this->setRoutes($routes);
+        $this->setTexts($texts);
     }
 
     /**
@@ -26,35 +33,58 @@ class Router
      * @return bool true
      */
     protected function setPath(
-        $path = ""
+        $path = null
     ) : bool {
+        if ($path === null) return false;
         $this->path = $path;
         return true;
     }
 
     /**
-     * Set a array of the availables routes edited in the config/routes.yaml file
+     * Set a array of the availables routes edited files passed as arguments
      * 
-     * @param null
+     * @param string|array $routes  String or array of strings of the yaml files paths to set the routes
      * @return bool true
      */
     protected function setRoutes(
-        $routesPath = ""
+        $routes = null
     ) : bool {
-        $this->routes = Helpers::getYaml($routesPath);
+        if ($routes === null) return false;
+        switch (gettype($routes)) 
+        {
+            case "string": 
+                $this->routes = Helpers::getYaml($routes);
+                break;
+            case "array": 
+                $this->routes = array_merge(...array_map(function($v){
+                    return Helpers::getYaml($v);
+                }, $routes));
+                break;
+        }
         return true;
     }
 
     /**
-     * Set a array of the texts set in the config/texts.yaml file
+     * Set a array of the texts set files passed as arguments
      * 
-     * @param null
+     * @param string|array $texts  String or array of strings of the yaml files paths to set the texts
      * @return bool true
      */
-    private function setTexts(
-        $textsPath = ""
+    protected function setTexts(
+        $texts = null
     ) : bool {
-        $this->texts = Helpers::getYaml($textsPath);
+        if ($texts === null) return false;
+        switch (gettype($texts)) 
+        {
+            case "string": 
+                $this->texts = Helpers::getYaml($texts);
+                break;
+            default: 
+                $this->texts = array_merge(...array_map(function($v){
+                    return Helpers::getYaml($v);
+                }, $texts));
+                break;
+        }
         return true;
     }
 }
