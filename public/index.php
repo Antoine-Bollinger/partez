2c7152fd7a7cb2a06784f883bf2896cda43b59ac
Antoine Bollinger <antoine.bollinger@gmail.com>
2024-03-05 13:10:04 +0100

    🐛 Fixes case sensitive bugs

--- public/index.php
+++ public/index.php
@@ -1,4 +1,4 @@
 <?php
-require_once __DIR__ . "/../vendor/autoload.php";
+require_once dirname(__DIR__) . "/vendor/autoload.php";
 
-new Abollinger\Partez\App\Starter();
+new \Abollinger\Partez\App\Starter();
