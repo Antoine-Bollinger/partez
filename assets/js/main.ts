cbe458711f83c359211cba0562060547e6ec2f56
Antoine Bollinger <antoine.bollinger@gmail.com>
2024-11-08 13:15:28 +0100

    💄 log partez

--- assets/js/main.ts
+++ assets/js/main.ts
@@ -1,8 +1,10 @@
 import copyCode from "./components/copyCode"
 import Login from "./views/login"
+import { Console } from "./utils/console"
 
 class App {
     static hello() {
+        console.log(`%c${Console.printLetters({ text: "partez" })}`, "color:#4F5B93;font-weight:bold;")
         console.log("🔥 Welcome to partez! 🔥\r\nPlease feel free to edit js in public/js directory.")
     }
 
