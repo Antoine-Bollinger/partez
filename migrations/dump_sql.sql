5259dccff623ca1d283be23630cc125735340331
Antoine Bollinger <antoine.bollinger@gmail.com>
2024-11-15 10:26:23 +0100

    ⬆️ bricolo update

--- migrations/dump_sql.sql
+++ migrations/dump_sql.sql
@@ -1,10 +1,9 @@
 DROP TABLE IF EXISTS `users`;
-CREATE TABLE IF NOT EXISTS `users` (
+CREATE TABLE `users` (
     `userId` int(3) NOT NULL,
     `password` varchar(255) NOT NULL,
-    `name` varchar(255) NOT NULL,
     PRIMARY KEY (`userId`)
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
 
-INSERT INTO `users` (`userId`, `password`, `name`) VALUES
-(:userId, :password, "Test");
+INSERT INTO `users` (`userId`, `password`) VALUES
+('999', '$2y$12$pSe7DWznXBHgvAZ9AtvJl.OxFhmX9694wFTmEJL5kKFsrmsb5uzzu');
