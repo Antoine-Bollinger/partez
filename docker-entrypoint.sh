e3b4186c4fde4a9648271bfa75dfa78abd053373
Antoine Bollinger <antoine.bollinger@gmail.com>
2025-04-30 09:51:06 +0200

    🐛 bug in permissions

--- docker-entrypoint.sh
+++ docker-entrypoint.sh
@@ -2,17 +2,16 @@
 
 # Wait for MySQL to be available on port 3306
 echo "⏳ Waiting for MySQL to be available at db:3306..."
-
 while ! nc -z db 3306; do
   sleep 1
 done
-
 echo "✅ MySQL is up!"
 
-# Ensure permissions and install dependencies
-chown -R www-data:www-data /var/www/html/vendor
-
+# Install php dependencies
 composer install
+
+# Ensure permissions
+chown -R www-data:www-data /var/www/html/vendor
 
 # Run migrations
 php bricolo migrate
